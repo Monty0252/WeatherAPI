92ce5dcc6168f52703d0156e79d791a92179f392
Muntasir Muin <mamuin@buffalo.edu>
2022-03-26 00:43:49 -0400

    Final Commit

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_weather():
         if temperature < 30:
             temp_text.config(fg="#0000FF")
         else:
-            temp_text.config(fg = "#FF3030")
+            temp_text.config(fg="#FF3030")
         desc_text.config(text=desc.capitalize())
         wind_text.config(text=f"{wind} mph")
         humidity_text.config(text=f"{humidity}%")
